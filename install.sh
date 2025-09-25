7933e14e7855e579c3330e94c099e4abd1c9c6f0
soerenosisa <soeren.maske@osisa.com>
2025-09-25 16:42:02 +0200

    changed order

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
+echo "" > /tmp/secret.key
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount disk.nix
 mount | grep /mnt
-nixos-generate-config --no-filesystems --root /mnt
-cp disk.nix /etc/nixos/
-cp flake.nix /etc/nixos/nixos-install --flake /mnt/etc/nixos/.#zmask
-echo "" > /tmp/secret.key
+#nixos-generate-config --no-filesystems --root /mnt
+cp -r . /mnt/etc/nixos/
+nixos-install --flake /mnt/etc/nixos/.#zmask
