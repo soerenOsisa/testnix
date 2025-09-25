51ac258ca9566e81d4314a4b646dbfdd35189a00
soerenosisa <soeren.maske@osisa.com>
2025-09-25 20:49:48 +0200

    need to gen

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "" > /tmp/secret.key
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount disk.nix
 mount | grep /mnt
-#nixos-generate-config --no-filesystems --root /mnt
+nixos-generate-config --no-filesystems --root /mnt
 cp -r . /mnt/etc/nixos/
 nixos-install --flake /mnt/etc/nixos/.#zmask
